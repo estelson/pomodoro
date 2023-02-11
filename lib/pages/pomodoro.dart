6cde7171c2f6143e56867be8fc9683175b8d6da0
Estelson Medeiros Pereira <estelson@gmail.com>
2023-02-11 17:10:18 -0300

    PomodoroStore - Alteração #01

--- lib/pages/pomodoro.dart
+++ lib/pages/pomodoro.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/components/cronometro.dart';
 import 'package:pomodoro/components/entrada.tempo.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
 
 class Pomodoro extends StatelessWidget {
   const Pomodoro({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<PomodoroStore>(context);
+
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text("Pomodoro"),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const [
-              EntradaTempo(titulo: "Trabalho", valor: 25,),
-              EntradaTempo(titulo: "Descanso", valor: 5,),
-            ],
+          Expanded(
+            child: Cronometro(),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                EntradaTempo(
+                  titulo: "Trabalho",
+                  valor: store.tempoTrabalho,
+                ),
+                EntradaTempo(
+                  titulo: "Descanso",
+                  valor: store.tempoDescanso,
+                ),
+              ],
+            ),
           ),
         ],
       ),
