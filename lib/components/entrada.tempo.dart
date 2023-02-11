4526c156926db5cf1de5476f325022a142930c70
Estelson Medeiros Pereira <estelson@gmail.com>
2023-02-11 16:17:35 -0300

    Alterações na classe Pomodoro e correção no alinhamento e distribuição dos componentes na tela

--- lib/components/entrada.tempo.dart
+++ lib/components/entrada.tempo.dart
@@ -9,9 +9,50 @@ class EntradaTempo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text(titulo),
-        Text(valor.toString()),
+        Text(
+          titulo,
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(15),
+                backgroundColor: Colors.red,
+              ),
+              child: Icon(
+                Icons.arrow_downward,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              "${valor.toString()} min",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(15),
+                backgroundColor: Colors.red,
+              ),
+              child: Icon(
+                Icons.arrow_upward,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
       ],
     );
   }
--- lib/pages/pomodoro.dart
+++ lib/pages/pomodoro.dart
@@ -8,10 +8,15 @@ class Pomodoro extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [
+        children: [
           Text("Pomodoro"),
-          EntradaTempo(titulo: "Trabalho", valor: 25,),
-          EntradaTempo(titulo: "Descanso", valor: 5,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              EntradaTempo(titulo: "Trabalho", valor: 25,),
+              EntradaTempo(titulo: "Descanso", valor: 5,),
+            ],
+          ),
         ],
       ),
     );
